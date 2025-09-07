ba183a20902df78e86cd647bcd5273abe3e803b5
Rupak Ghosh <rupakghosh10101@gmail.com>
2025-09-07 15:29:54 +0530

    eighth

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+const API_URL = "https://fraud-news-main-398a736.kuberns.cloud";
+
+export default API_URL;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import API_URL from "../config";
+
 
 function Register() {
   const [form, setForm] = useState({ username: "", email: "", password: "" });
